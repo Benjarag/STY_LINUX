7882b3a96ea3c9ed24266d539f97e5b44f1eb73b
Benjamín Ragnarsson <benjarag7@outlook.com>
2025-01-29 10:07:40 +0000

    list.c lab 3.2

--- STY25/vika3/tutorial-03/list.c
+++ STY25/vika3/tutorial-03/list.c
@@ -13,23 +13,67 @@ struct QueueItem *listHead = NULL;
 
 void appendItem(void *task)
 {
+    struct QueueItem *newNode = (struct QueueItem *)malloc(sizeof(struct QueueItem));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newNode->task = task; // append the task to the new node
+    newNode->next = NULL;
+
+    if (!listHead) {
+        listHead = newNode;
+        return;
+    }
+
+    struct QueueItem *tempItem = listHead;
+
+    while (tempItem->next) {
+        tempItem = tempItem->next;
+    }
+    tempItem->next = newNode;
     //...implement this
     // append to the end of the list
 }
 
 void *removeFirstItem()
 {
+    if (!listHead) {
+        return NULL;
+    }
+    
+    void *temp_task = listHead->task;
+
+    struct QueueItem *nextItem = listHead->next;
+
+    free(listHead);
+
+    listHead = nextItem;
+    
+    return temp_task;
+
     //implement this
     // removes the first list item from the list and returns its value; returns NULL if list is empty
 }
 
 int containsItem(void *task)
 {
+    struct QueueItem *tempItem = listHead;
+    while (tempItem) {
+        if (tempItem->task == task)
+            return 1;
+        tempItem = tempItem->next;
+    }
+    return 0;
     //implement this
     // return true(1) if list contains value, false(0)
 }
 
 int isEmpty() {
+    if (listHead == NULL) {
+        return 0;
+    }
+    return 1;
     // implement this
     // return true (1) if list is empty, false (0) otherwise
 }
